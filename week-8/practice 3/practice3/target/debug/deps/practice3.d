2c96720ce42a1a5a124cb5244a7810d9fcc11af4
GitTheCommitMaster <sunday.olayiwola@pau.edu.ng>
2025-11-19 19:20:36 +0100

    Delete week-8/practice 3/practice3 directory

--- week-8/practice 3/practice3/target/debug/deps/practice3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\NEW\Documents\s.olayiwolaCSC101\week-8\practice 3\practice3\target\debug\deps\practice3.d: src\main.rs
-
-C:\Users\NEW\Documents\s.olayiwolaCSC101\week-8\practice 3\practice3\target\debug\deps\practice3.exe: src\main.rs
-
-src\main.rs:
